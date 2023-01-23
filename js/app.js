7d14ce0dab2ae454865bb2d7adb9823cce7e77bf
rajddutta <rajdeepdutta074@gmail.com>
2023-01-23 13:47:01 +0530

    initial commit

---
+++ js/app.js
@@ -0,0 +1,29 @@
+
+// ============nav toggle========
+const navBtn = document.getElementById("nav-toggle");
+const links = document.getElementById("nav-links");
+//  add event listener
+navBtn.addEventListener("click", () => {
+  links.classList.toggle("show-links");
+});
+
+// --- smooth scroll-----
+const scrollLinks = document.querySelectorAll(".scroll-link");
+scrollLinks.forEach(link => {
+  link.addEventListener("click", e => {
+    e.preventDefault();
+    links.classList.remove("show-links");
+
+    const id = e.target.getAttribute("href").slice(1);
+    const element = document.getElementById(id);
+
+    let position = element.offsetTop - 62;
+
+    window.scrollTo({
+      left: 0,
+      // top: element.offsetTop,
+      top:position,
+      behavior: "smooth"
+    });
+  });
+});
